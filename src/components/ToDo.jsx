6041af88afd5dd4fb82f53b728982a316c6c31bd
Didier Dakoury <dazibao76@gmail.com>
2021-11-25 09:19:52 +0000

    third commit

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,14 +4,14 @@ function ToDo(props) {
 
     return (
         <div className="todo">
+            <div className="task--checking">
+                <input type="checkbox" />
+            </div>
             <div className="task--title">
                 {props.name}
             </div>
-            <div className="task--checking">
-
-            </div>
             <div className="task--postponing">
-                
+                <i class="fas fa-pencil-alt"></i>
             </div>
             <div className="task--deleting">
                 <i className="fas fa-trash"></i>
